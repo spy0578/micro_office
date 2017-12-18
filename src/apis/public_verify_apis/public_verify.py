a34b0295a6a54474ee34a9876c3e66e258cfc8e9
spy0578 <spy0578@promote.cache-dns.local>
2017-12-18 22:06:33 +0800

    流程设计

--- src/apis/public_verify_apis/public_verify.py
+++ src/apis/public_verify_apis/public_verify.py
@@ -42,17 +42,26 @@ class BasePostClass(MethodView):
         
         '''
         判断输入的是否是手机号
-        如果是则根据手机号查找notes信息表，查找到notes的userid，再和当前的from_user_name比较
-        如果是则注册成功：登记注册信息表
+	如果注册信息表有记录，则说明已注册过
+        否则根据手机号查找notes信息表，查找到notes的userid，再和当前的from_user_name比较
+        如果是则注册成功：登记注册信息表, 设置默认权限
         '''
 
-        if input_content.isdigit():
-            #设置用户权限为
-            #update_access_token(const.PUBLIC_PLAT_CLIENT_ID, const.PUBLIC_PLAT_CLIENT_SECRET)
-            modify_user_group(from_user_name, '432')
+	'''
+	触发菜单点击事件，获取菜单名，根据用户notesuserid判断是否已注册，从公众号服务端获取用户组信息，
+	菜单与用户组 权限关系表 用户组1-菜单1
+                                用户组1-菜单2
+                                用户组2-菜单2
+        如果查询到权限关系，则可以访问
+        推送图文信息
+        '''
+
+        if input_content.isdigit() and len(input_content) == 11 :
 
 
-
+            #设置用户权限为const.OTHER_DEP_GROUP
+            if modify_user_group(from_user_name, const.OTHER_DEP_GROUP) is False:
+            	return False, {}, json.dumps({"msgtype":"text","touser":from_user_name,"text":{"content":"注册失败，请再次尝试"}})
 
             return True, {}, json.dumps({"msgtype":"text","touser":from_user_name,"text":{"content":"手机号注册成功"}})
 
